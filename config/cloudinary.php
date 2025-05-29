73a7cf477f1c28b12de6695544474dbc2bbfab99
Zedphilism <zaidi3116@gmail.com>
2025-05-30 00:10:22 +0800

    Fix: correct Cloudinary API key

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 \Cloudinary\Configuration\Configuration::instance([
   'cloud' => [
     'cloud_name' => 'dvslefxk7',
-    'api_key'    => '51964786417983',
+    'api_key'    => '519647864817983',  // ← corrected key
     'api_secret' => 'B4kGIx56yziJ6o3rVxGWsvFjIIk',
   ],
   'url' => [
