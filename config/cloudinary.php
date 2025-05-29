a616f4ae534f9c0b73431e48b3fc82dd75fa1cbc
Zedphilism <zaidi3116@gmail.com>
2025-05-29 23:58:33 +0800

    Connect Cloudinary to admin image uploads

---
+++ config/cloudinary.php
@@ -0,0 +1,13 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+\Cloudinary\Configuration\Configuration::instance([
+  'cloud' => [
+    'cloud_name' => 'dvslefxk7',
+    'api_key'    => '51964786417983',
+    'api_secret' => 'B4kGIx56yziJ6o3rVxGWsvFjIIk',
+  ],
+  'url' => [
+    'secure' => true
+  ]
+]);
