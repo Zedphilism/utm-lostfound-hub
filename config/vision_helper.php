aedbed5754d1cc1bf3b4d4f8689db6ef1fd286dc
Munzir_Mdn <munzir.mdn@gmail.com>
2025-06-20 01:22:42 +0800

    Add files via upload
    google vision config

---
+++ config/vision_helper.php
@@ -0,0 +1,41 @@
+<?php
+require_once __DIR__ . '/config.php';
+
+function getVisionLabels($imagePath) {
+    global $GOOGLE_API_KEY;
+
+    $imageData = file_get_contents($imagePath);
+    $encodedImage = base64_encode($imageData);
+
+    $json = json_encode([
+        'requests' => [[
+            'image' => ['content' => $encodedImage],
+            'features' => [['type' => 'LABEL_DETECTION', 'maxResults' => 5]],
+        ]],
+    ]);
+
+    $url = 'https://vision.googleapis.com/v1/images:annotate?key=' . $GOOGLE_API_KEY;
+
+    $ch = curl_init($url);
+    curl_setopt_array($ch, [
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_POST => true,
+        CURLOPT_HTTPHEADER => ['Content-Type: application/json'],
+        CURLOPT_POSTFIELDS => $json,
+    ]);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    $result = json_decode($response, true);
+    $labels = [];
+
+    if (isset($result['responses'][0]['labelAnnotations'])) {
+        foreach ($result['responses'][0]['labelAnnotations'] as $annotation) {
+            $labels[] = $annotation['description'];
+        }
+    }
+
+    return implode(', ', $labels);
+}
+?>
